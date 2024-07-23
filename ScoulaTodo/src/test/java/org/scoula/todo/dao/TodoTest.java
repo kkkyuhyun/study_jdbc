aa15a9d20bd4cfb597fb741f86137bc537c8ce15
김규현 <sam99571@kakao.com>
2024-07-23 16:53:35 +0900

    Add files via upload

---
+++ ScoulaTodo/src/test/java/org/scoula/todo/dao/TodoTest.java
@@ -0,0 +1,36 @@
+package org.scoula.todo.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoTest {
+
+    @Test
+    void getTotalCount() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getList() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
